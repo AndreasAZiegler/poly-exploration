51087d3343358dbf649a688688a2f96fd9276201
Andreas Ziegler <a.ziegler@mt-robot.com>
2018-12-28 07:24:43 +0100

    Added pose header.

---
+++ include/pose.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <kindr/minimal/quat-transformation.h>
+
+typedef kindr::minimal::QuatTransformation Pose;
+typedef kindr::minimal::RotationQuaternion Rotation;
+typedef Eigen::Vector3d Position;
