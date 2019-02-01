2aa6ef778174b5adb645daf71753c64ef2f1c28e
Andreas Ziegler <a.ziegler@mt-robot.com>
2019-02-01 19:25:03 +0100

    Added (un)equality operators for Point.

--- include/Point.h
+++ include/Point.h
@@ -3,11 +3,32 @@
 
 #pragma once
 
+#include <cmath>
+
 struct Point {
   Point(double x, double y) {
     x_ = x;
     y_ = y;
   }
+
+  inline bool operator==(const Point& point) {
+    return (fabs(this->x_ - point.x_) < 1e-4) &&
+           (fabs(this->y_ - point.y_) < 1e-4);
+  }
+
+  inline friend bool operator==(const Point& lhs, const Point& rhs) {
+    return (fabs(lhs.x_ - rhs.x_) < 1e-4) && (fabs(lhs.y_ - rhs.y_) < 1e-4);
+  }
+
+  inline bool operator!=(const Point& point) {
+    return (fabs(this->x_ - point.x_) > 1e-4) ||
+           (fabs(this->y_ - point.y_) > 1e-4);
+  }
+
+  inline friend bool operator!=(const Point& lhs, const Point& rhs) {
+    return (fabs(lhs.x_ - rhs.x_) > 1e-4) || (fabs(lhs.y_ - rhs.y_) > 1e-4);
+  }
+
   double x_;
   double y_;
 };
