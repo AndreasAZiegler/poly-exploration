e884c4113a9eab01425831d3b87282ed02a39fac
Andreas Ziegler <062.127@gmail.com>
2019-02-02 16:24:40 +0100

    Extended Point class and added tests.

---
+++ src/Point.cpp
@@ -0,0 +1,24 @@
+#include "Point.h"
+
+Point::Point(double x, double y) {
+  x_ = x;
+  y_ = y;
+}
+
+bool Point::operator==(const Point& point) {
+  return (fabs(this->x_ - point.x_) < 1e-4) &&
+         (fabs(this->y_ - point.y_) < 1e-4);
+}
+
+bool operator==(const Point& lhs, const Point& rhs) {
+  return (fabs(lhs.x_ - rhs.x_) < 1e-4) && (fabs(lhs.y_ - rhs.y_) < 1e-4);
+}
+
+bool Point::operator!=(const Point& point) {
+  return (fabs(this->x_ - point.x_) > 1e-4) ||
+         (fabs(this->y_ - point.y_) > 1e-4);
+}
+
+bool operator!=(const Point& lhs, const Point& rhs) {
+  return (fabs(lhs.x_ - rhs.x_) > 1e-4) || (fabs(lhs.y_ - rhs.y_) > 1e-4);
+}
